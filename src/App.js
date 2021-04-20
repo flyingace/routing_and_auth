cef547fa8f1b0c43e6df80a0a3759d2efaa3ae6f
David Cameron <dc@davidcameron.biz>
2021-04-20 16:33:56 -0400

    Example of protected route POC Later versions will connect to authentication service

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Redirect,
+  useHistory,
+  useLocation
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100) // fake async
+  }
 }
 
-export default App;
+const Home = () => <h3>Home</h3>
+const Notifications = () => <h3>Notifications</h3>
+
+function Login() {
+  const [
+    redirectToReferrer,
+    setRedirectToReferrer
+  ] = React.useState(false)
+
+  const { state } = useLocation()
+
+  const login = () => fakeAuth.authenticate(() => {
+    setRedirectToReferrer(true)
+  })
+
+  if (redirectToReferrer === true) {
+    return <Redirect to={state?.from || '/'} />
+  }
+
+  return (
+    <div>
+      <p>You must log in to view the page</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  )
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route {...rest} render={({ location }) => {
+      return fakeAuth.isAuthenticated === true
+        ? children
+        : <Redirect to={{
+          pathname: '/login',
+          state: { from: location }
+        }} />
+    }} />
+  )
+}
+
+function AuthButton () {
+  const history = useHistory()
+
+  return fakeAuth.isAuthenticated === true
+    ? <p>
+      Welcome! <button onClick={() => {
+      fakeAuth.signout(() => history.push('/'))
+    }}>Sign out</button>
+    </p>
+    : <p>You are not logged in.</p>
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <AuthButton />
+
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/notifications">Notifications</Link></li>
+        </ul>
+
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute path='/notifications'>
+          <Notifications/>
+        </PrivateRoute>
+      </div>
+    </Router>
+  )
+}
